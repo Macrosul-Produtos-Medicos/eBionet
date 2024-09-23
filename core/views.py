9eb84b9af01195b376c3d26f033a2e4bae906bc8
renan <renan.silveira@macrosul.com>
2024-09-23 10:47:03 -0300

    view das paginas de contato,suporte, e produtos

--- core/views.py
+++ core/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def suporte(request):
+    return render(request,'suporte.html')
+
+def contato(request):
+    return render(request,'contato.html')
+
+def eletro(request):
+    return render(request,'eletro.html')
+
+def monitor(request):
+    return render(request,'monitor.html')
+
+def medidor(request):
+    return render(request,'medidor.html')
