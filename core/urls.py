039ddc5aaa890387a9c9d07c9d0580c7153874ed
renan <renan.silveira@macrosul.com>
2024-09-23 10:46:54 -0300

    urls das paginas de contato,suporte, e produtos

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
-from .views import home
+from .views import home,suporte, contato, eletro, monitor, medidor
 
 urlpatterns = [
     path('', home, name="home"),
+    path('suporte', suporte, name="suporte"),
+    path('contato', contato, name="contato"),
+
+    path('produtos/eletro', eletro, name="eletro"),
+    path('produtos/monitor', monitor, name="monitor"),
+    path('produtos/medidor', medidor, name="medidor"),
 ]
