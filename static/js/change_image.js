07b5828ab11e616190fa51f2aaf5208c0cf26ae7
renan <renan.silveira@macrosul.com>
2024-09-23 15:33:50 -0300

    variaveis de medidor

--- static/js/change_image.js
+++ static/js/change_image.js
@@ -1,6 +1,10 @@
 const imgCardio7 = document.querySelector('#img-cardio-7')
 const imgCardioCare2000 = document.querySelector('#img-cardio-care-2000')
 const imgCardiotouch3000 = document.querySelector('#img-cardiotouch-3000')
+const bm1 = document.querySelector('#img-bm1')
+const bm3 = document.querySelector('#img-bm3')
+const bm5 = document.querySelector('#img-bm5')
+
 
 function trocaDeImagem(variavel, img1, img2) {
     variavel.addEventListener('mouseover', () => {
@@ -26,10 +30,5 @@ function trocaDeImagem(variavel, img1, img2) {
     });
 }
 
-trocaDeImagem(imgCardio7, 'https://i.postimg.cc/jjK6RFGH/Eletrocardi-grafo-Cardio7-Bionet-completo.jpg', 'https://s3.sa-east-1.amazonaws.com/macrosul.media/itens/eletrocardiografo/cardio7-dicom/I03368/eletrocardiografo-cardio7-dicom-bionet-I03368-5.png')
-
-trocaDeImagem(imgCardioCare2000,'https://i.postimg.cc/HkZHmVvd/cardiocare-2000-acessorios.jpg','https://s3.sa-east-1.amazonaws.com/macrosul.media/itens/eletrocardiografo/cardiocare-2000/I04007/eletrocardiografo-cardiocare-2000-bionet-I04007-1.png')
-
-trocaDeImagem(imgCardiotouch3000,'https://i.postimg.cc/BQSdvJK6/Eletrocardiografo-Cardio-Touch-3000-acessorios.jpg','https://s3.sa-east-1.amazonaws.com/macrosul.media/itens/eletrocardiografo/cardiotouch/I04145/eletrocardiografo-cardiotouch-bionet-I04145-1.png')
 
 
