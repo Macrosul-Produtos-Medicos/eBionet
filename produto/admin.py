014bb9b5f4315eab718fc06f2570274ef4e2ef04
Giordano Henrique Silveira <giordano.silveira@macrosul.com>
2024-10-25 11:49:03 -0300

    feat: Colocando os modelos no admin Coloquei os modelos criados no admin vide commit: 7b7cd33

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    ArquivoConteudoApoio,
+    ConteudoApoio,
+    ImagemProduto,
+    Modelo,
+    Produto,
+    Subcategoria,
+    VideoProduto,
+)
+
+class ArquivoConteudoApoioInLine(admin.TabularInline):
+    model = ArquivoConteudoApoio
+    extra = 0
+    readonly_fields = ('criado_em', 'modificado_em')
+
+
+@admin.register(ConteudoApoio)
+class ConteudoApoioAdmin(admin.ModelAdmin):
+    inlines = (ArquivoConteudoApoioInLine,)
+    list_display = ('__str__', 'nome', 'modelo')
+    readonly_fields = ('criado_em', 'modificado_em')
+
+
+@admin.register(Modelo)
+class ModeloAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'nome', 'subcategoria', 'imagem_principal')
+    readonly_fields = ('criado_em', 'modificado_em')
+    list_editable = ('nome', 'imagem_principal', 'subcategoria')
+    
+class ImagemProdutoInline(admin.TabularInline):
+    model = ImagemProduto
+    extra = 0
+    readonly_fields = ('criado_em', 'modificado_em')
+    
+    
+class VideoProdutoInline(admin.TabularInline):
+    model = VideoProduto
+    extra = 0
+    readonly_fields = ('criado_em', 'modificado_em')
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = (ImagemProdutoInline, VideoProdutoInline)
+    list_display = (
+        'slug',
+        'nome',
+        'cod_sap_item',
+        'modelo',
+        'ean',
+    )
+    readonly_fields = ('criado_em', 'modificado_em')
+    list_filter = ('modelo',)
+    search_fields = ('nome',)
+    list_editable = (
+        'nome',
+        'cod_sap_item',
+        'modelo',
+        'ean',
+    )
+           
+@admin.register(Subcategoria)
+class SubcategoriaAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'nome', 'slug')
+    readonly_fields = ('criado_em', 'modificado_em', 'slug')
+    list_editable = (
+        'nome',
+    )
+
