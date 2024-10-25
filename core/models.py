565436c7f3db190a95a6d43be1a79be4939b383f
Giordano Henrique Silveira <giordano.silveira@macrosul.com>
2024-10-25 09:28:41 -0300

    feat: Modelos base para todos os produtos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class UuidModel(models.Model):
+    slug = models.UUIDField(unique=True, editable=False, default=uuid.uuid4)
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampedModel(models.Model):
+    criado_em = models.DateTimeField('criado em', auto_now_add=True, auto_now=False)
+    modificado_em = models.DateTimeField('modificado em', auto_now_add=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def created_display(self):
+        from backend.core.utils import datetime_to_string
+
+        return datetime_to_string(self.created, '%d/%m/%y')
+
+
+class Ativo(models.Model):
+    ativo = models.BooleanField('ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseModel(TimeStampedModel, Ativo):
+    class Meta:
+        abstract = True
