aecff00012aa7303a37ff7631b7c1b2b86bb8167
Giordano Henrique Silveira <giordano.silveira@macrosul.com>
2024-11-04 09:06:41 -0300

    feat: Comentando urls que não usarei por enquanto e trocando a url do admin, para prover mais segurança

--- bionet/urls.py
+++ bionet/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-suporte-ebionet-com-br/', admin.site.urls),
     path('', include('produto.urls')),
-    path('', include('core.urls')),
+    # path('', include('core.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
